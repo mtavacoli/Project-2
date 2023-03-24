8854e2ad719a9a470f9d401e768605cdebe42340
Matin Tavacoli <mtavacoli33@gmail.com>
2023-03-23 19:59:16 -0500

    controllers

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,20 +1,5 @@
 const router = require("express").Router();
-const { User } = require("../models");
-
-router.post("/", async (req, res) => {
-  try {
-    const userData = await User.create(req.body);
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.status(200).json(userData);
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+const { User } = require("../../models/user");
 
 router.post("/login", async (req, res) => {
   try {
@@ -23,7 +8,7 @@ router.post("/login", async (req, res) => {
     if (!userData) {
       res
         .status(400)
-        .json({ message: "Incorrect email or password, please try again!" });
+        .json({ message: "Incorrect email or password, please try again" });
       return;
     }
 
@@ -32,7 +17,7 @@ router.post("/login", async (req, res) => {
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: "Incorrect email or password, please try again!" });
+        .json({ message: "Incorrect email or password, please try again" });
       return;
     }
 
